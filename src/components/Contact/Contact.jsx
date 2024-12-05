d3bfa4ee17668e185fc5ff04e89ee5570b8bc8d1
Masudur400 <md8004210@gmail.com>
2024-12-05 18:26:47 +0600

    project add dynamicly

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,8 +4,8 @@ import { MdOutlineLocationOn, MdOutlineMail } from "react-icons/md";
 
 const Contact = () => {
     return (
-        <div className="my-10 text-white text-opacity-80">
-            <h3 className="text-2xl md:text-3xl font-bold text-center">Contact Me</h3>
+        <div className="my-14 text-white text-opacity-80">
+            <h3 className="text-2xl md:text-4xl font-medium text-center">Contact Me</h3>
             <div data-aos="zoom-in" className=" shadow-md shadow-gray-400 p-3 lg:w-2/5 md:w-3/5 mx-auto mt-8 rounded-md">
                 <div>
                     <p className="flex gap-1 md:text-xl items-center"><MdOutlineMail></MdOutlineMail> Email : md8004210@gmail.com
