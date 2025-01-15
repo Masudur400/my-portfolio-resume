ea22b2f73e4724e941b4a2fe599cdb17767bc83c
Masudur400 <md8004210@gmail.com>
2025-01-15 22:24:49 +0600

    linkedin link add

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
                         <p className="text-center text-white text-xl font-medium my-4 underline">Socials</p>
                         <div className="grid grid-flow-col gap-4">
                             <a href="https://www.facebook.com/MD.RANA.MIA.VHAI" target="blank"><FaFacebookSquare className="text-4xl text-blue-500"></FaFacebookSquare></a>
-                            <a href="https://www.linkedin.com/in/masudur-rahman-profile/" target="blank"><FaLinkedin className="text-4xl text-sky-500"></FaLinkedin ></a>
+                            <a href="https://www.linkedin.com/in/md-masudur-rahman-profile/" target="blank"><FaLinkedin className="text-4xl text-sky-500"></FaLinkedin ></a>
                             <a href="https://github.com/Masudur400" target="blank"><FaGithub className="text-4xl text-gray-400"></FaGithub  ></a>
                         </div>
                     </nav>
