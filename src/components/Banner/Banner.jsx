2845d2932dd5b7bc79e86b806e5f6e921562420f
Masudur400 <md8004210@gmail.com>
2025-02-17 15:51:40 +0600

    banner margin fixed

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
         <div className="avg w-full max-sm:h-[680px]">
             <div className='bg-black bg-opacity-80 h-full py-10'>
                 <div className='md:flex gap-9 items-center '>
-                    <img src={img2} alt="" data-aos="zoom-in" className='h-72 w-72 border-2 rounded-full lg:ml-20 md:ml-10 mx-1 md:mt-8' />
+                    <img src={img2} alt="" data-aos="zoom-in" className='h-72 w-72 border-2 rounded-full lg:ml-20 md:ml-10 md:mx-1 mx-auto md:mt-8' />
                     <div className='text-white text-opacity-80 opacity-90 max-sm:ml-4 lg:w-3/6 md:w-2/4 mt-3' data-aos="zoom-in">
                         <p className='lg:text-3xl md:text-2xl text-xl font-bold'>Hi !</p>
                         <p className='lg:text-4xl md:text-3xl text-2xl font-bold lg:ml-12 md:ml-12 mb-3'>I am <span className='text-indigo-500'>Masudur Rahman</span></p> 
